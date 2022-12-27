ab0c867dabfaa12396629e680bb550db1f2cf473
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2022-12-27 11:13:03 +0530

    Finish 02_repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if(num>0){
+    let str_1=str;
+    for(let i=1;i<num;i++) {
+        str_1+=str;
+    }
+    return str_1;}
+    else if(num==0){
+        return '';
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
