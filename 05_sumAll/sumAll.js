f823e63d29b863a4d64afaf38070e140fcb7944f
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2022-12-27 19:08:35 +0530

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(n1,n2) {
+    if(typeof n1 === 'number' && typeof n2 === 'number'){
+    if(n1>0&&n2>0){
+        let n=0;
+        if(n2>n1){
+            for(;n1<=n2;n1++){
+                n+=n1;
+            }
+            return n;
+        }
+        else {
+            for(;n2<=n1;n2++){
+                n+=n2;
+            }
+            return n;
+        }
+    }
+    else return 'ERROR';
+}
+    else return 'ERROR';
+    
 
 };
 
