4b5ed522489bcc7801cf36361d240d5f15fb0645
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2022-12-27 11:35:54 +0530

    Finish 03-reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const str_1=str.split("");
+    const str_2=[];
+    for(let i=0;i<str_1.length;i++) {
+        str_2[i]=str_1[str_1.length-1-i];
+    }
+    const str_new=str_2.join("");
+    return str_new;
 
 };
 
