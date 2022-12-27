a2ddd0a047463d9bfff856046b21838aa110ede7
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2022-12-27 19:42:10 +0530

    Finsih 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp_far) {
+  const temp_cel=(temp_far-32)*5/9;
+  return (Math.round(temp_cel*10)/10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp_cel) {
+  const temp_far=(temp_cel*9)/5 + 32;
+  return (Math.round(temp_far*10)/10);
 };
 
 // Do not edit below this line
