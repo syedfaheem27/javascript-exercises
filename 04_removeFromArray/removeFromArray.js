47ec3ab3475d16051264dffbb26b2eae78040dbc
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2022-12-27 15:19:33 +0530

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...arr_new) {
+    for(let i=0;i<arr_new.length;i++){
+        let j=0;
+        for(;j<arr.length;j++){
+            if(arr[j]===arr_new[i]){
+                arr.splice(j,1);
+                break;
+            }
+        }
 
+    }
+    return arr;
 };
 
 // Do not edit below this line
