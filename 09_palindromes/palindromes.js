076350a16ba23e4410825591243ae4e4e8a39ffb
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-13 16:26:05 +0530

    Finsished 09-Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let str_new=str.toUpperCase();
+    let arr=str_new.split('');
+    let len=arr.length;
+    let result=true;
+    for(let i=0;i<len;i++) {
+        if(arr[i]===' '||arr[i]=='.'||arr[i]=='!'||arr[i]==','){
+            arr.splice(i,1);
+            i--;
+        }
+    }
+    for(let i=0;i<arr.length;i++) {
+        if(arr[i]!==arr[arr.length-i-1]){
+            result=false;
+            break;
+        }
+    }
+    return result;
 
 };
 
